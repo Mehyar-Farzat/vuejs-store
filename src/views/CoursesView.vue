b5d94cc727794302ce32f8daf2db4eeed217c982
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-30 00:14:49 +0100

    create components and import them in courses view.vue

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,9 +1,24 @@
 <template>
   <h2>Courses Page</h2>
+  <div>
+    <CourseList/>
+  </div>
+
+  <div>
+    <CourseFilter/>
+  </div>
+
 </template>
 
 <script>
+
+  import CourseList from '@/components/CoursList.vue'
+  import CourseFilter from '@/components/CourseFilter.vue'
   export default {
-    name: 'CoursesPage'
+    name: 'CoursesPage',
+    components:{
+      CourseList,
+      CourseFilter
+    }
   }
 </script>
