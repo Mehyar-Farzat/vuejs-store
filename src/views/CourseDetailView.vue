aaca56523081ad8fc9df6530b718e840518984ae
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-29 23:45:25 +0100

    update courses view, update coursedetail view, update index.js, update app.vue

---
+++ src/views/CourseDetailView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Course Detail Page</h2>
+</template>
+
+<script>
+  export default {
+    name: 'CourseDetailPage'
+  }
+</script>
---
+++ src/views/CoursesView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Courses Page</h2>
+</template>
+
+<script>
+  export default {
+    name: 'CoursesPage'
+  }
+</script>
