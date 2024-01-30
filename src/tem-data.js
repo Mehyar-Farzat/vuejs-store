d5c6d06214617b77136d0d8859c438b2ae7e5210
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-30 20:02:33 +0100

    create tem-data.js file and add dummy data

---
+++ src/tem-data.js
@@ -0,0 +1,38 @@
+import img1 from './assets/1.jpeg'
+import img2 from './assets/2.jpeg'
+import img3 from './assets/3.jpeg'
+import img4 from './assets/4.jpeg'
+
+
+
+export const courses = [
+    {
+        id:1 ,
+        name: 'Python Basics' ,
+        category: 'Web Development' ,
+        category_id : 1 ,
+        image: img1
+    },
+
+    {
+        id:2 ,
+        name: 'Html & Css Basics' ,
+        category: 'Software Development' ,
+        category_id : 2 ,
+        image: img2
+    },
+    {
+        id:3 ,
+        name: 'AI Development' ,
+        category: 'Machine Learning' ,
+        category_id : 3 ,
+        image: img3
+    },
+    {
+        id:4 ,
+        name: 'Ios & Android Basics' ,
+        category: 'Mobile Development' ,
+        category_id : 4 ,
+        image: img4
+    },
+]
