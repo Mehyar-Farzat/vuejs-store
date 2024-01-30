bcf8effac6930ffa39884e646c4989f0d3e610fe
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-30 20:58:00 +0100

    build simple grid system template for course detail

--- src/views/CourseDetailView.vue
+++ src/views/CourseDetailView.vue
@@ -1,5 +1,17 @@
 <template>
-  <h2>Course Detail Page</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <img :src="course.image" alt="">
+      </div>
+      <div class="col-lg-6">
+        <h2>{{ course.name }}</h2>
+        <h4>{{ course.category }}</h4>
+        <p>{{ course.prise }}$</p>
+        <button>Enroll</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
