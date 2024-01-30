feaca6f6f98bb48bd7214cb53152fb063c1e2a43
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-30 19:46:53 +0100

    update course list.vue

--- src/components/CoursList.vue
+++ src/components/CoursList.vue
@@ -1,10 +1,11 @@
 <template>
 <div class="row mt-5">
-  <div class="col-lg-3">
+  <div class="col-lg-3" v-for="course in courses" :key="courses.id">
     <div class="card">
-      <img src="..." class="card-img-top" alt="...">
+      <img :src="course.image" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">{{course.name}}</h5>
+        <p>{{ course.category }}</p>
         <RouterLink class="btn btn-primary" to="/course-detail">Course Detail</RouterLink>
       </div>
     </div>
@@ -14,6 +15,12 @@
 
 <script>
   export default {
-    name: 'CourseList'
+    name: 'CourseList',
+    data(){
+      return{
+        courses: []
+      }
+      
+    }
   }
 </script>
