882ee7bfb96046d05bd74caba65e8a7970fb5e3f
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-30 18:57:49 +0100

    link course detail into card

--- src/components/CoursList.vue
+++ src/components/CoursList.vue
@@ -1,11 +1,11 @@
 <template>
-<div class="row">
+<div class="row mt-5">
   <div class="col-lg-3">
     <div class="card">
       <img src="..." class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
-        <a href="#" class="btn btn-primary">Course Details</a>
+        <RouterLink class="btn btn-primary" to="/course-detail">Course Detail</RouterLink>
       </div>
     </div>
   </div>
