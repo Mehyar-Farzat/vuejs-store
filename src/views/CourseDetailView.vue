4071f61d5a442078c9936407f9439f4220d7f0c9
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-30 21:14:52 +0100

    update course detail view page

--- src/views/CourseDetailView.vue
+++ src/views/CourseDetailView.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row my-5">
       <div class="col-lg-6">
-        <img :src="course.image" alt="">
+        <img :src="course.image" class="img-fluid" alt="">
       </div>
       <div class="col-lg-6">
         <h2>{{ course.name }}</h2>
         <h4>{{ course.category }}</h4>
         <p>{{ course.prise }}$</p>
-        <button>Enroll</button>
+        <button class="btn btn-secondary">Enroll</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { course } from '../tem-data'
+import { course, courses } from '../tem-data'
   export default {
-    name: 'CourseDetailPage'
+    name: 'CourseDetailPage',
+    data(){
+      return {
+        course: courses.find(course => course.id == this.$route.params.course_id)
+      }
+    }
   }
 </script>
